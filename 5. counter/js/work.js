4a2faadff2eb5e6d26551d2617845e83b9d4264a
RajeevKumarSinha <rajeev.exousia@gmail.com>
2021-03-30 21:27:18 +0530

    adding color to -ve and +ve number

--- 5. counter/js/work.js
+++ 5. counter/js/work.js
@@ -2,13 +2,26 @@
 //this function subtracts 1 from the count
 function lowCount(){
     var count=parseInt(document.getElementById('num').textContent);
-    document.getElementById('num').innerHTML=count-1;
+    count-=1;
+    if(count==0){
+        document.getElementById('num').style.color='black';
+    }
+    if(count<0){
+    document.getElementById('num').style.color="red";
+}
+    document.getElementById('num').innerHTML=count;
 }
 
 //this function adds 1 to the count
 
 function addCount(){
     var count=parseInt(document.getElementById('num').textContent);
+    count+=1;
+    
+    if(count==0)
+    {document.getElementById('num').style.color="black";}
+    if(count>=1){
+    document.getElementById('num').style.color="green";}
     // alert(count);
-    document.getElementById('num').innerHTML=count+1;
+    document.getElementById('num').innerHTML=count;
 }
